30d8132771bfa2209bdde06203f3f92449d8a87f
DevMehta22 <mehtadev907@gmail.com>
2024-05-21 17:49:18 +0530

    creating and handling json data

--- 21json/main.go
+++ 21json/main.go
@@ -15,7 +15,8 @@ type course struct{
 
 func main()  {
 	fmt.Println("create and handling JSON")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 
 
 }
@@ -36,4 +37,42 @@ func EncodeJson()  {
 		}
 	
 	fmt.Println(string(finaljson))
+}
+
+func DecodeJson()  {
+	jsonData := []byte(`
+	{
+		"coursename": "Go Programming",
+		"Price": 100,
+		"website": "Udemy",
+		"tags": [
+				"Programming",
+				"Go"
+		]
+}
+	`)
+
+	var cc course 
+
+	checkValid := json.Valid(jsonData)
+
+	if checkValid {
+		fmt.Println("Json is valid")
+		json.Unmarshal(jsonData, &cc)
+		fmt.Printf("%#v\n",cc) 		// # is used to print the type of data and v is used to print the actual value of that data
+	}else{
+		fmt.Println("JSON is invalid")
+	}
+
+	//sone cases were data is to be added as key value pair
+
+	var myOnlineData map[string]interface{}
+	
+	json.Unmarshal(jsonData,&myOnlineData)
+	fmt.Printf("%#v\n",myOnlineData)
+
+	for k,v := range myOnlineData {
+		fmt.Printf("key is %v and value is %v and Type is %T\n",k,v,v)
+	}
+
 }
